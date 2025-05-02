05f50cdff8791d50ccecb86f009b36926829aa51
diasfaga <faga05@gmail.com>
2025-05-02 19:01:34 +0200

    main.py

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 import os
 from flask import Flask, request
-import requests
-
-TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '✅ Bot rodando.'
+    return '✅ Bot rodando (teste).'
 
-@app.route(f'/{TOKEN}', methods=['POST'])
-def webhook():
+
+@app.route('/<token>', methods=['POST'])
+def webhook(token):
     data = request.get_json()
-    chat_id = data['message']['chat']['id']
-    text = data['message']['text']
-
-    if text == '/start':
-        send_message(chat_id, '✅ Bot de futuros MEXC online e funcionando!')
-    else:
-        send_message(chat_id, f'Você disse: {text}')
-
+    print('📥 Recebido do Telegram:', data)
     return 'ok'
 
-def send_message(chat_id, text):
-    payload = {'chat_id': chat_id, 'text': text}
-    requests.post(TELEGRAM_API_URL, json=payload)
 
 if __name__ == '__main__':
     from waitress import serve
